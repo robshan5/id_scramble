476c4f58674e87789f9abcb325b82ef3a6084ef8
robshan5 <robshanahan5@proton.me>
2025-07-01 10:02:18 +0100

    fixed bug, now uses stdin rather than fileinput

--- scramble-id.py
+++ scramble-id.py
@@ -1,12 +1,18 @@
 import argparse
-import fileinput
 import random
 import re
+import sys
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-m", "--mapping", help="declares the mapping of digits", required=True)
-parser.add_argument("-s", "--step", help="declares the step going through the map (default=1) [must be odd]")
+parser.add_argument(
+    "-m", "--mapping", help="declares the mapping of digits", required=True
+)
+parser.add_argument(
+    "-s",
+    "--step",
+    help="declares the step going through the map (default=1) [must be odd]",
+)
 
 args = parser.parse_args()
 mapping = args.mapping
@@ -19,11 +25,13 @@ if len(mapping) != 10:
     print("Please enter map of length 10 for all digits")
     exit(1)
 
+
 # iterate through all lines of the file
-for line in fileinput.input():
+for line in sys.stdin:
+    line = line.strip()
     match = re.search(r"^\d{7,8}", line)
 
-    modifier = random.randrange(0,10)
+    modifier = random.randrange(0, 10)
 
     # if there's a match, replace the id with the scrambled id
     if match != None:
@@ -32,7 +40,7 @@ for line in fileinput.input():
             if value != "/":
                 # id[index] = str((int(value) + 2) % 10)
                 id[index] = mapping[(int(value) + modifier) % 10]
-                modifier = (modifier+step) % 10
+                modifier = (modifier + step) % 10
             else:
                 break
         id = "".join(id)
